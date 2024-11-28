f039b0eadf87af641a6080a4883c6b9f1a8f4240
SamueleTerraroli <terraroli99@gmail.com>
2024-11-28 14:37:57 +0100

    build: add base file

---
+++ assets/script/script.js
@@ -0,0 +1,38 @@
+const teamMembers = [
+    {
+      name: "Marco Bianchi",
+      role: "Designer",
+      email: "marcobianchi@team.com",
+      img: "male1.png"
+    },
+    {
+      name: "Laura Rossi",
+      role: "Front-end Developer",
+      email: "laurarossi@team.com",
+      img: "female1.png"
+    },
+    {
+      name: "Giorgio Verdi",
+      role: "Back-end Developer",
+      email: "giorgioverdi@team.com",
+      img: "male2.png"
+    },
+    {
+      name: "Marta Ipsum",
+      role: "SEO Specialist",
+      email: "martarossi@team.com",
+      img: "female2.png"
+    },
+    {
+      name: "Roberto Lorem",
+      role: "SEO Specialist",
+      email: "robertolorem@team.com",
+      img: "male3.png"
+    },
+    {
+      name: "Daniela Amet",
+      role: "Analyst",
+      email: "danielaamet@team.com",
+      img: "female3.png"
+    }
+  ];
